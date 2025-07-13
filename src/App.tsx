4db2e8f1ebaee33459de05e737543d87f50f4453
Neeraj Gulati <neeraj.gulati@wynk.in>
2025-07-14 01:32:10 +0530

    store created via createStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import { Provider } from 'react-redux';
+
+import store from './redux/store';
 
 
 function App() {
